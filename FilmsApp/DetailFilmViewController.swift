b067c84d63af2187655debf53b0bc22281b0c844
Тузов Николай <tuzovns@gmail.com>
2022-08-10 18:37:43 +0300

    Добавлена кастовая анимация открытия постера Изменена цветовая схема приложения

--- FilmsApp/DetailFilmViewController.swift
+++ FilmsApp/DetailFilmViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class DetailFilmViewController: UIViewController {
+class DetailFilmViewController: UIViewController, UIViewControllerTransitioningDelegate {
 
     @IBOutlet weak var posterImageView: UIImageView!
     @IBOutlet weak var filmTitleLabel: UILabel!
@@ -17,6 +17,7 @@ class DetailFilmViewController: UIViewController {
     @IBOutlet weak var descriptionTextView: UITextView!
 
     var destinationIndex: Int = Int()
+    var transition: RoundingTransition = RoundingTransition()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +28,32 @@ class DetailFilmViewController: UIViewController {
         rateLabel.text = testArray[destinationIndex].testRating
     }
 
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transition.transitionProfile = .show
+        transition.start = posterImageView.center
+        transition.roundColor = UIColor(red: 0, green: 42/255, blue: 92/255, alpha: 0.5)
+
+        return transition
+    }
+
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transition.transitionProfile = .cancel
+        transition.start = posterImageView.center
+        transition.roundColor = UIColor(red: 0, green: 42/255, blue: 92/255, alpha: 0.5)
+
+        return transition
+    }
+
     @IBAction func tapGestureAction(_ sender: UITapGestureRecognizer) {
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let fullPicPosterVC = segue.destination as? PosterFullViewController else { return }
+
+        fullPicPosterVC.detailIndexPath = destinationIndex
+
+        fullPicPosterVC.transitioningDelegate = self
+        fullPicPosterVC.modalPresentationStyle = .custom
     }
 
     /*
--- FilmsApp/PosterFullViewController.swift
+++ FilmsApp/PosterFullViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 class PosterFullViewController: UIViewController {
 
     @IBOutlet weak var fullPosterImageView: UIImageView!
+    @IBOutlet weak var closeButton: UIButton!
+
+    var detailIndexPath: Int = Int()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        fullPosterImageView.image = UIImage(named: testArray[detailIndexPath].testPic ?? "image1")
         // Do any additional setup after loading the view.
+    }
+    @IBAction func closeButtonTouchUpInside(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
     /*
     // MARK: - Navigation
